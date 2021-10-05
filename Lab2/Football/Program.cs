6e7ea62a6593fc6f2831625a75827aa0e5be9c59
Paul <gigi.boss52@gmail.com>
2021-10-05 08:51:14 +0300

    Update

--- Lab2/Football/Program.cs
+++ Lab2/Football/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace Weather
+namespace Football
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hello World!");
         }
     }
 }
